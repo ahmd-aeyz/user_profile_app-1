c9d5a2853d30ff2fcbe4167d2b73e8589790e699
Omar Assem <omarassem996@gmail.com>
2022-03-27 14:42:04 +0200

    fourth commit

--- lib/features/profile/presentation/screens/edit_profile_screen.dart
+++ lib/features/profile/presentation/screens/edit_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:final_project/core/domain/error/error_toast.dart';
 import 'package:final_project/core/presentation/validation/validators.dart';
 import 'package:final_project/core/presentation/widgets/custom_elevated_button.dart';
 import 'package:final_project/core/presentation/widgets/custom_text_form_field.dart';
@@ -30,105 +31,95 @@ class EditProfileScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: BlocConsumer<ProfileCubit, ProfileState>(
-            listener: (context, state) {
-              if (state is ProfileSuccess) {
-                Navigator.of(context).popUntil((route) => route.isFirst);
-                Navigator.of(context)
-                    .pushReplacementNamed(ViewProfileScreen.routeName);
-              }
-            },
+          child: BlocBuilder<ProfileCubit, ProfileState>(
             builder: (context, state) {
-              return state.maybeWhen(
-                loading: () => const Center(child: CircularProgressIndicator()),
-                error: (error) => Center(child: Text(error)),
-                // success: (user) {
-
-                //   return Container();
-                // },
-                orElse: () =>
-                    //Form(
-                    // key: _formKey,
-                    // child:
-                    Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        imageFile = await _getImage();
-                      },
-                      child: const Text('Change profile image'),
+              bool isLoading = false;
+              state.maybeWhen(
+                loading: () => isLoading = true,
+                error: (error) => showErrorToast(errorMessage: error),
+                success: (_) {
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                  Navigator.of(context)
+                      .pushReplacementNamed(ViewProfileScreen.routeName);
+                },
+                orElse: () {},
+              );
+              return Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      imageFile = await _getImage();
+                    },
+                    child: const Text('Change profile image'),
+                  ),
+                  CustomTextFormField(
+                    controller: nameController,
+                    hintText: 'Change name',
+                    keyboardType: TextInputType.name,
+                    validator: (value) => generalValidator(
+                      value: value,
+                      fieldName: 'Name',
                     ),
-                    CustomTextFormField(
-                      controller: nameController,
-                      hintText: 'Change name',
-                      keyboardType: TextInputType.name,
-                      validator: (value) => generalValidator(
-                        value: value,
-                        fieldName: 'Name',
-                      ),
-                      prefixIcon: Icons.person_outline,
+                    prefixIcon: Icons.person_outline,
+                  ),
+                  CustomTextFormField(
+                    controller: emailController,
+                    hintText: 'Change email address',
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) => emailValidator(value),
+                    prefixIcon: Icons.email_outlined,
+                  ),
+                  CustomTextFormField(
+                    controller: passwordController,
+                    hintText: 'Change password',
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value) => passwordValidator(value),
+                    prefixIcon: Icons.lock_outline,
+                  ),
+                  CustomTextFormField(
+                    controller: phoneController,
+                    hintText: 'Change phone number',
+                    keyboardType: TextInputType.phone,
+                    validator: (value) => phoneValidator(value),
+                    prefixIcon: Icons.phone_outlined,
+                  ),
+                  CustomTextFormField(
+                    controller: addressController,
+                    hintText: 'Change address',
+                    keyboardType: TextInputType.name,
+                    validator: (value) => generalValidator(
+                      value: value,
+                      fieldName: 'Address',
                     ),
-                    CustomTextFormField(
-                      controller: emailController,
-                      hintText: 'Change email address',
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) => emailValidator(value),
-                      prefixIcon: Icons.email_outlined,
+                    prefixIcon: Icons.home_work_outlined,
+                  ),
+                  CustomTextFormField(
+                    controller: ageController,
+                    hintText: 'Change age',
+                    keyboardType: TextInputType.number,
+                    validator: (value) => generalValidator(
+                      value: value,
+                      fieldName: 'Age',
                     ),
-                    CustomTextFormField(
-                      controller: passwordController,
-                      hintText: 'Change password',
-                      keyboardType: TextInputType.visiblePassword,
-                      validator: (value) => passwordValidator(value),
-                      prefixIcon: Icons.lock_outline,
-                    ),
-                    CustomTextFormField(
-                      controller: phoneController,
-                      hintText: 'Change phone number',
-                      keyboardType: TextInputType.phone,
-                      validator: (value) => phoneValidator(value),
-                      prefixIcon: Icons.phone_outlined,
-                    ),
-                    CustomTextFormField(
-                      controller: addressController,
-                      hintText: 'Change address',
-                      keyboardType: TextInputType.name,
-                      validator: (value) => generalValidator(
-                        value: value,
-                        fieldName: 'Address',
-                      ),
-                      prefixIcon: Icons.home_work_outlined,
-                    ),
-                    CustomTextFormField(
-                      controller: ageController,
-                      hintText: 'Change age',
-                      keyboardType: TextInputType.number,
-                      validator: (value) => generalValidator(
-                        value: value,
-                        fieldName: 'Age',
-                      ),
-                      prefixIcon: Icons.manage_accounts_outlined,
-                    ),
-                    const SizedBox(height: 16),
-                    CustomElevatedButton(
-                      label: 'submit',
-                      onPressed: () {
-                        // if (_formKey.currentState!.validate()) {
-                        BlocProvider.of<ProfileCubit>(context).editProfile(
-                          name: nameController.text,
-                          email: emailController.text,
-                          password: passwordController.text,
-                          phone: phoneController.text,
-                          imageFile: imageFile,
-                          address: addressController.text,
-                          age: ageController.text,
-                        );
-                      },
-                      //},
-                    ),
-                  ],
-                ),
-                // ),
+                    prefixIcon: Icons.manage_accounts_outlined,
+                  ),
+                  const SizedBox(height: 16),
+                  CustomElevatedButton(
+                    label: 'submit',
+                    onPressed: () {
+                      BlocProvider.of<ProfileCubit>(context).editProfile(
+                        name: nameController.text,
+                        email: emailController.text,
+                        password: passwordController.text,
+                        phone: phoneController.text,
+                        imageFile: imageFile,
+                        address: addressController.text,
+                        age: ageController.text,
+                      );
+                    },
+                    isLoading: isLoading,
+                  ),
+                ],
               );
             },
           ),
